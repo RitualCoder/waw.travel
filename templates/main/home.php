cb05bcdbd019841c4004d006a64f79000924c960
Laura-Klery <lklery@yahoo.fr>
2024-01-17 17:48:19 +0100

    Fix icons link and folder

--- templates/main/home.php
+++ templates/main/home.php
@@ -6,15 +6,15 @@
   <section class="last-add-travel">
     <h2>Nos derniers ajouts</h2>
     <article>
-      <img src="/public/uploads/images/placeholder.png" alt="nom" >
+      <img src="/uploads/images/placeholder.png" alt="nom" >
       <div class="travel-card">
         <h3>Nom</h3>
         <div>
-          <span><img src="/icons/compass.svg" alt="compas"> valeur km</span>
-          <span><img src="/public/icons/crossroads.svg" alt="panneau de direction"> valeur étapes</span>
+          <span><img src="/images/icons/compass.svg" alt="compas"> valeur km</span>
+          <span><img src="/images/icons/crossroads.svg" alt="panneau de direction"> valeur étapes</span>
         </div>
-        <span><img src="/public/icons/location.svg" alt="localisation"> De : valeur</span>
-        <span><img src="/public/icons/location.svg" alt="localisation"> À : valeur</span>
+        <span><img src="/images/icons/location.svg" alt="localisation"> De : valeur</span>
+        <span><img src="/images/icons/location.svg" alt="localisation"> À : valeur</span>
       </div>
 
     </article>
